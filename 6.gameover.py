a8c7c88db324c5e308530a19178a180b4e347a43
샘이깊은물 <samgib@naver.com>
2021-01-12 17:02:38 +0900

    1st commit

--- 6.gameover.py
+++ 6.gameover.py
@@ -75,6 +75,16 @@ balls.append({
 weapon_to_remove = -1
 ball_to_remove = -1
 
+# 폰트 정의
+
+game_font = pygame.font.Font(None, 40)
+total_time = 100
+start_ticks = pygame.time.get_ticks()
+
+# 게임 종료 메세지 /
+# Mission Complete
+game_result = "Game Over"
+
 running = True
 while running:
     dt = clock.tick(30)
@@ -199,7 +209,10 @@ while running:
                         "init_spd_y": ball_speed_y[ball_img_idx + 1]  # y 최초 속도
                     })
 
-                break
+                break# 안쪽 for 문 탈출
+        else:
+            continue # 안쪽 for문 조건이 맞지 않으면 continue 바깥 for문
+        break
 
         # 충돌된 공 or 무기 없애기
         if ball_to_remove > -1:
@@ -209,6 +222,10 @@ while running:
         if weapon_to_remove > -1:
             del weapons[weapon_to_remove]
             weapon_to_remove = -1
+        # 모든 공을 없앤 경우 게임 종료
+        if len(balls) == 0:
+            game_result = "Mission Complete"
+            running = False
 
     # 화면에 그리기
     screen.blit(background, (0, 0))
@@ -225,6 +242,24 @@ while running:
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
 
+    # 경과 시간 계산
+    elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000
+    timer = game_font.render("Time: {}".format(int(total_time - elapsed_time)), True, (255, 255, 255))
+    screen.blit(timer, (10, 10))
+
+    if total_time - elapsed_time <= 0:
+        game_result = "Time Over"
+        running = False
+
     pygame.display.update()
+
+# 게임 오버 메세지
+
+msg = game_font.render(game_result, True, (255, 255, 0))  # 노란색
+msg_rect = msg.get_rect(center=(int(screen_width / 2), int(screen_height / 2)))
+screen.blit(msg, msg_rect)
+pygame.display.update()
+#2초대기
+pygame.time.delay(2000)
 
 pygame.quit()
